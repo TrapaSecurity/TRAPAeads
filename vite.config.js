b65fe014f073e9039b5b07f5bc2345a87d7521b4
ddaa <0xddaa@users.noreply.github.com>
2025-07-04 10:40:39 +0800

    Update vite.config.js
    use domain

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/TRAPAeads/' : '/',
+  base: '/',
   server: {
     port: 3000
   },
